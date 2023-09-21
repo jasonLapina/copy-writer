ee9d7b249523083ca430e9d307f6daf8addc6157
jasonLapina <lapina.jason@gmail.com>
2023-09-21 16:38:47 +0800

    add custom theme, add contacts

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,46 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, HStack, Heading, Image, Text } from "@chakra-ui/react";
 function Contact() {
+  const contacts = [
+    {
+      text: "jmgswipes@gmail.com",
+      icon: "/gmail.png",
+    },
+    {
+      text: "https://www.linkedin.com/in/jmgwrites",
+      icon: "/linkedin.png",
+    },
+  ];
   return (
-    <Box mt='120px' minH='100vh'>
-      <Heading textAlign='center'>You can reach me via:</Heading>
+    <Box mt='120px' minH='400px'>
+      <Heading>You can reach me via:</Heading>
+      <HStack gap='80px' mt='40px'>
+        {contacts.map((item) => (
+          <Box
+            as='a'
+            href={item.text}
+            rel='noreferrer noopener'
+            target='_blank'
+            key={item.text}
+          >
+            <Image
+              w='200px'
+              borderRadius='full'
+              src={item.icon}
+              alt={item.text}
+              cursor='pointer'
+            />
+            <Button
+              fontSize='20px'
+              fontWeight='bold'
+              textAlign='center'
+              variant='link'
+              color='blackAlpha.700'
+            >
+              {item.text}
+            </Button>
+          </Box>
+        ))}
+      </HStack>
     </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import AOS from "aos";
 
 import "aos/dist/aos.css";
@@ -13,9 +13,18 @@ AOS.init({
   easing: "ease-in-out",
 });
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#3f88c5",
+      400: "#65a0d1",
+      700: "#326d9e",
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
 );
