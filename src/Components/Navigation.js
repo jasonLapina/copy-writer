63beae411da88bb06d4b5637e4c4727d92ad5585
jasonLapina <lapina.jason@gmail.com>
2023-09-23 19:19:50 +0800

    finish media queries

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,4 +1,18 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  useMediaQuery,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 function Navigation() {
@@ -27,6 +41,8 @@ function Navigation() {
 
   const [isPastHero, setIsPastHero] = useState(false);
 
+  const [isSmallScreen] = useMediaQuery("(max-width: 768px)");
+
   const handleScroll = () => {
     if (window.scrollY >= window.innerHeight - 100) setIsPastHero(true);
     else {
@@ -37,6 +53,8 @@ function Navigation() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box
       bgColor='blue.200'
@@ -52,54 +70,96 @@ function Navigation() {
         opacity: 1,
       }}
     >
-      <HStack
-        justifyContent='space-between'
-        alignItems='center'
-        alignContent='center'
-        justifyItems='center'
-        maxW='1800px'
-        mx='auto'
-      >
-        <Button
-          _hover={{
-            color: "secondary",
-          }}
-          color='white'
-          variant='link'
-          pl='120px'
-          onClick={() => {
-            document.getElementById("home").scrollIntoView({
-              behavior: "smooth",
-            });
-          }}
-        >
-          Home
-        </Button>
+      {/* DRAWER NAV */}
+      {isSmallScreen && (
+        <>
+          <Button pl='16px' colorScheme='secondary' onClick={onOpen}>
+            Menu
+          </Button>
+          <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+            <DrawerOverlay />
+            <DrawerContent maxW='fit-content'>
+              <DrawerCloseButton color='red' />
+              <DrawerHeader mb='80px'>Navigate</DrawerHeader>
+              <DrawerBody>
+                <VStack gap='16px' alignItems='start' justify='end'>
+                  {sections.map((item) => (
+                    <Button
+                      fontSize='24px'
+                      color='secondary'
+                      key={item.link}
+                      variant='link'
+                      _hover={{
+                        color: "secondary",
+                      }}
+                      onClick={() => {
+                        document.getElementById(item.link).scrollIntoView({
+                          behavior: "smooth",
+                        });
+                        onClose();
+                      }}
+                    >
+                      {item.text}
+                    </Button>
+                  ))}
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </>
+      )}
+
+      {!isSmallScreen && (
         <HStack
+          justifyContent='space-between'
           alignItems='center'
           alignContent='center'
-          gap='32px'
-          justifyContent='end'
+          justifyItems='center'
+          maxW='1800px'
+          mx='auto'
+          px='16px'
         >
-          {sections.map((item) => (
-            <Button
-              color='white'
-              key={item.link}
-              variant='link'
-              _hover={{
-                color: "secondary",
-              }}
-              onClick={() => {
-                document.getElementById(item.link).scrollIntoView({
-                  behavior: "smooth",
-                });
-              }}
-            >
-              {item.text}
-            </Button>
-          ))}
+          <Button
+            _hover={{
+              color: "secondary",
+            }}
+            color='white'
+            variant='link'
+            pl={{ base: "0px", lg: "120px" }}
+            onClick={() => {
+              document.getElementById("home").scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
+          >
+            Home
+          </Button>
+          <HStack
+            alignItems='center'
+            alignContent='center'
+            gap='32px'
+            justifyContent='end'
+          >
+            {sections.map((item) => (
+              <Button
+                color='white'
+                key={item.link}
+                variant='link'
+                _hover={{
+                  color: "secondary",
+                }}
+                onClick={() => {
+                  document.getElementById(item.link).scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                {item.text}
+              </Button>
+            ))}
+          </HStack>
         </HStack>
-      </HStack>
+      )}
     </Box>
   );
 }
